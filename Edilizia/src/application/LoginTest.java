489e158c14118a575f6d76ad51563725ae97099a
ilaria <ilariacialfi@gmail.com>
2019-02-04 07:47:32 +0100

    commit mattina
    test

---
+++ Edilizia/src/application/LoginTest.java
@@ -0,0 +1,19 @@
+package application;
+
+import static org.junit.Assert.*;
+
+import java.sql.SQLException;
+
+import org.junit.Test;
+
+import control.LoginController;
+
+public class LoginTest {
+
+	@Test
+	public void testDoAccess() throws ClassNotFoundException, SQLException {
+		assertFalse(LoginController.accedi("id", "pass"));
+		assertTrue(LoginController.accedi("11", "01"));
+	}
+
+}
